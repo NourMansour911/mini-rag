70bbba61d070c53e23f642636cf9f6f9a5d788e1
Nour Mansour <seriousnoursf90@gmail.com>
2026-03-12 17:36:06 +0200

    edit vdb shcema

--- src/schemas/vdb_schema.py
+++ src/schemas/vdb_schema.py
@@ -4,11 +4,11 @@ from typing import List,Optional
 
 class PushRequest(BaseModel):
     do_reset: Optional[int] = 0
-    files_names: Optional[list[str]]
+    files_names: Optional[list[str]] = None
     
     
 class PushResponse(BaseModel):
     no_of_inserted_chunks: int
     signal: str
-    no_of_files: Optional[int] 
-    files_names: Optional[list[str]]
+    no_of_files: Optional[int] = None
+    files_names: Optional[list[str]] = None
